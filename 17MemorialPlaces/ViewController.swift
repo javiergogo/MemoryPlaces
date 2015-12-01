6306ec9a43487e669e90cba9869b21883f620293
Javier Gomez Glez <javier.go.go@hotmail.com>
2015-12-01 12:55:05 -0800

    Initial Commit

---
+++ 17MemorialPlaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  17MemorialPlaces
+//
+//  Created by Javier Gomez on 12/1/15.
+//  Copyright © 2015 Javier Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
